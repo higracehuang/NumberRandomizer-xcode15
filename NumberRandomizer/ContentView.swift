9f213426b6b3ff71b7ea905b7d85437678744fb0
higracehuang <higracehuang@gmail.com>
2024-01-19 11:21:38 -0600

    Clean up and add text

--- NumberRandomizer/ContentView.swift
+++ NumberRandomizer/ContentView.swift
@@ -1,19 +1,9 @@
-//
-//  ContentView.swift
-//  NumberRandomizer
-//
-//  Created by Le Huang on 1/19/24.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+          Text("?")
         }
         .padding()
     }
