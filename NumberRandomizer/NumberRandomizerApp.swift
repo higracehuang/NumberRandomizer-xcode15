b3ae69c53a2c5b8e86887b8f1bbec7ba55946164
higracehuang <higracehuang@gmail.com>
2024-01-19 10:46:39 -0600

    Initial Commit

---
+++ NumberRandomizer/NumberRandomizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NumberRandomizerApp.swift
+//  NumberRandomizer
+//
+//  Created by Le Huang on 1/19/24.
+//
+
+import SwiftUI
+
+@main
+struct NumberRandomizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
