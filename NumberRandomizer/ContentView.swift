4346d739d012a844842fa6f311fef1a0bc1c1eaa
higracehuang <higracehuang@gmail.com>
2024-01-19 13:42:16 -0600

    Random number logic

--- NumberRandomizer/ContentView.swift
+++ NumberRandomizer/ContentView.swift
@@ -1,19 +1,21 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-          Text("?")
-          Button {
-            
-          } label: {
-            Text("Generate Random Number")
-          }
-        }
-        .padding()
+  @State private var randomNum: Int?
+  
+  var body: some View {
+    VStack {
+      Text(randomNum != nil ? "\(randomNum!)" : "?")
+      Button {
+        randomNum = Int.random(in: 1...100)
+      } label: {
+        Text("Generate Random Number")
+      }
     }
+    .padding()
+  }
 }
 
 #Preview {
-    ContentView()
+  ContentView()
 }
