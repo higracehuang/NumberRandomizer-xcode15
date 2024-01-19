27901b699059cec64eb19cbf0b2d3f5589f075e7
higracehuang <higracehuang@gmail.com>
2024-01-19 14:43:27 -0600

    Style the UI

--- NumberRandomizer/ContentView.swift
+++ NumberRandomizer/ContentView.swift
@@ -6,10 +6,14 @@ struct ContentView: View {
   var body: some View {
     VStack {
       Text(randomNum != nil ? "\(randomNum!)" : "?")
+        .font(.system(size: 30))
+        .padding()
       Button {
         randomNum = Int.random(in: 1...100)
       } label: {
         Text("Generate Random Number")
+          .padding()
+          .font(.system(size: 20))
       }
     }
     .padding()
